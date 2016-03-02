55d578f3ab2336f3e416a57bf5d01e129bc082eb
Rakesh <rakeshbhatia87@gmail.com>
2016-03-02 08:34:51 -0800

    Assignment 30 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,14 @@ posts = Post.all
   )
 end
 
+puts "#{Post.count} posts created"
+Post.find_or_create_by(title: "Post Title", body: "Post Body")
+puts "#{Post.count} posts created"
+
+puts "#{Comment.count} comments created"
+Comment.find_or_create_by(post: Post.find_or_create_by(title: "Post Title", body: "Post Body"), body: "Comment Body")
+puts "#{Comment.count} comments created"
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
