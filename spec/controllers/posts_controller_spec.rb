27b723b592ff580f217734d33ca4f15eb91f1b40
Rakesh <rakeshbhatia87@gmail.com>
2016-03-05 18:07:21 -0800

    Assignment 33 completed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe PostsController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
+      
       expect(response).to have_http_status(:success)
     end
-  end
+    it "assigns [my_post] to @posts" do
+      get :index
 
-  it "assigns [my_post] to @posts" do
-    get :index
-
-    expect(assigns(:posts)).to eq([my_post])
+      expect(assigns(:posts)).to eq([my_post])
+    end
   end
 
   describe "GET show" do
