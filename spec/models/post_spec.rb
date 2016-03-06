2a748ff4f9d410500550903872e3503b877e1b3d
Rakesh <rakeshbhatia87@gmail.com>
2016-03-06 07:34:29 -0800

    Checkpoint 34 rails topics and posts: completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-    let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
-    describe "attributes" do
-      it "responds to title" do
-        expect(post).to respond_to(:title)
-      end
-      it "responds to body" do
-        expect(post).to respond_to(:body)
-      end
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+
+  it { is_expected.to belong_to(:topic) }
+
+  describe "attributes" do
+    it "responds to title" do
+      expect(post).to respond_to(:title)
     end
+    it "responds to body" do
+      expect(post).to respond_to(:body)
+    end
+  end
 end
