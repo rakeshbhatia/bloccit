b7d7117d2d7b9d786d8a3b2712f707e32c6daa91
Rakesh <rakeshbhatia87@gmail.com>
2016-03-07 14:49:03 -0800

    Assignment 34 partially completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :sponsored_posts do
+    resources :topics, except: [:index]
+  end
+
   resources :topics do
     resources :posts, except: [:index]
   end
