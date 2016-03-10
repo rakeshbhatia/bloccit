88ceaf11433159f1f774a0a3a5a44a397ecf9cea
Rakesh <rakeshbhatia87@gmail.com>
2016-03-10 10:31:43 -0800

    Checkpoint 40 rails authorization: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
 
   before_save { self.email = email.downcase }
+  before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -13,4 +14,6 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  enum role: [:member, :admin]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -33,9 +33,16 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'rakeshbhatia87@gmail.com',
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
 )
 
