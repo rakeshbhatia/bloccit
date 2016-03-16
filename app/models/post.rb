b6a1a0b3a85952e3fa1d9f928827523ddee7baeb
Rakesh <rakeshbhatia87@gmail.com>
2016-03-16 11:20:08 -0700

    Assignment 44 partially completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,6 +15,7 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+  after_create :create_favorite
 
   def up_votes
     votes.where(value: 1).count
@@ -32,5 +33,10 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  def create_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 end
