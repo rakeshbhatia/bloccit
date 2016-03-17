2f49986c6714d49db941cb311779f33f9f6c1a6f
Rakesh <rakeshbhatia87@gmail.com>
2016-03-17 11:16:08 -0700

    Assignment 46 partially completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,9 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
-
   has_many :labelings, as: :labelable
-
   has_many :labels, through: :labelings
 
   scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :publicly_viewable, -> (user) { user ? all : where(public: true) }
+  scope :privately_viewable, -> (user) { user ? all : where(public: true) }
 end
