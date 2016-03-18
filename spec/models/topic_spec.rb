293a4ffed71c0972db9cb8eb091c59aad89a6f85
Rakesh <rakeshbhatia87@gmail.com>
2016-03-18 12:59:32 -0700

    Assignment 46 completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -27,7 +27,6 @@ RSpec.describe Topic, type: :model do
     end
   end
 
-
   describe "scopes" do
     before do
       @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
@@ -42,8 +41,19 @@ RSpec.describe Topic, type: :model do
       end
 
       it "returns only public topics if user is nil" do
-
         expect(Topic.visible_to(nil)).to eq([@public_topic])
+      end
+    end
+
+    describe "publicly_viewable" do
+      it "returns a collection of public topics" do
+        expect(Topic.publicly_viewable).to eq([@public_topic])
+      end
+    end
+
+    describe "privately_viewable" do
+      it "returns a collection of private topics" do
+        expect(Topic.privately_viewable).to eq([@private_topic])
       end
     end
   end
