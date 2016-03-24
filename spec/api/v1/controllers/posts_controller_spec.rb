a5e7c9addfdf5aabe267701aab92bea3c12e61d7
Rakesh <rakeshbhatia87@gmail.com>
2016-03-23 20:19:44 -0700

    Assignment 48 partially completed

---
+++ spec/api/v1/controllers/posts_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+ RSpec.describe Api::V1::PostsController, type: :controller do
+   let(:my_user) { create(:user) }
+   let(:my_topic) { create(:topic) }
+   let(:my_post) { create(:post) }
+   let(:my_comment) { Comment.create!(body: RandomData.random_paragraph, post: my_post, user: my_user) }
+
+   context "unauthenticated user" do
+     it "GET index returns http success" do
+       get :index
+
+       expect(response).to have_http_status(:success)
+     end
+
+     it "GET show returns http success" do
+       get :show, id: my_post.id
+
+       expect(response).to have_http_status(:success)
+     end
+
+     it "GET show returns array of child comments" do
+       get :show, id: my_post.id
+       response_hash = JSON.parse response.body
+       
+       expect(response_hash['comments']).to_not be_nil
+     end
+   end
+
+   context "unauthorized user" do
+
+   end
+ end
